7f57d43aabb2f4b6702bc4976804e7b477e83256
jazcala <jazcalabrese@gmail.com>
2025-06-24 09:28:26 -0300

    Initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,54 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: "https://www.saucedemo.com/",
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+    {
+      name: "all-browsers-and-tests",
+      use: {
+        ...devices["Desktop Chrome"],
+        baseURL: "https://www.saucedemo.com/",
+      },
+    },
+    {
+      name: "all-browsers-and-tests",
+      use: {
+        ...devices["Desktop Safari"],
+        baseURL: "https://www.saucedemo.com/",
+      },
+    },
+    {
+      name: "all-browsers-and-tests",
+      use: {
+        ...devices["Desktop Firefox"],
+        baseURL: "https://www.saucedemo.com/",
+      },
+    },
+  ],
+});
